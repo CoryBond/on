3d6532494dfd5bc41e1d4fa84dacaa62fe9cd258
Cory <cb.cobond@gmail.com>
2017-09-30 17:54:23 -0400

    f

--- build/bundle.js
+++ build/bundle.js
@@ -14839,7 +14839,7 @@ var ContactsPage = function (_React$Component) {
             // Email was not put in.
             if (!this.state.isEmailValid) return;
 
-            _smtp2.default.send("cory.bond.dev@gmail.com", "cb.cobond@gmail.com", "CoryBond.me - Mail", this.generateBody(), { token: "237807d0-013d-4391-85cd-796e9297e6ca" });
+            _smtp2.default.send("cory.bond.dev@gmail.com", "cb.cobond@gmail.com", "CoryBond.me - Mail", this.generateBody(), { token: "bb530d75-3b0e-42a7-a295-28f9eacf95d0" });
 
             //if(confirmation){
             this.handleShowSnackbar();
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -61,7 +61,7 @@ export default class ContactsPage extends React.Component {
             "cb.cobond@gmail.com",
             "CoryBond.me - Mail",
             this.generateBody(),
-            {token: "237807d0-013d-4391-85cd-796e9297e6ca"});
+            {token: "bb530d75-3b0e-42a7-a295-28f9eacf95d0"});
 
         //if(confirmation){
             this.handleShowSnackbar();
