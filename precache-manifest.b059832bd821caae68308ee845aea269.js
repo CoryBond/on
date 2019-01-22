ca0e35df8063ebd83f3652a220faa98634a7d921
Cory <cb.cobond@gmail.com>
2019-01-21 19:29:30 -0500

    Updates

---
+++ precache-manifest.b059832bd821caae68308ee845aea269.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6550ee360ba0c6a7acd447dbd23faba",
+    "url": "/on/static/media/GiphFind.f6550ee3.png"
+  },
+  {
+    "revision": "42f33b2288bf4400b916",
+    "url": "/on/static/css/main.6bbc14f0.chunk.css"
+  },
+  {
+    "revision": "8039edc9031e753372d2",
+    "url": "/on/static/js/1.8039edc9.chunk.js"
+  },
+  {
+    "revision": "c7944878d6989c8f2a79",
+    "url": "/on/static/js/runtime~main.c7944878.js"
+  },
+  {
+    "revision": "6a9170e65a81e5a761e347f01211ea4c",
+    "url": "/on/static/media/me_banner.6a9170e6.jpg"
+  },
+  {
+    "revision": "61339a837688e685a9ec5d3e625990c0",
+    "url": "/on/static/media/velocity.61339a83.jpg"
+  },
+  {
+    "revision": "16eb2261517b08d4bd21fc52ddbf8ec7",
+    "url": "/on/static/media/citibank.16eb2261.gif"
+  },
+  {
+    "revision": "a7027ebb450bd00e0e83e1cc490ee3a5",
+    "url": "/on/static/media/AVLighthouse.a7027ebb.PNG"
+  },
+  {
+    "revision": "42f33b2288bf4400b916",
+    "url": "/on/static/js/main.42f33b22.chunk.js"
+  },
+  {
+    "revision": "851115c3bb9d49277d056dc8aa1bec5a",
+    "url": "/on/static/media/MineSweeper_Thumbnail.851115c3.png"
+  },
+  {
+    "revision": "488a46d4acf50d399bb624e347a6d6d2",
+    "url": "/on/static/media/InfiniteScroll.488a46d4.png"
+  },
+  {
+    "revision": "ae3b6063940bdc78221974e991ae74c5",
+    "url": "/on/static/media/internetWorld.ae3b6063.jpg"
+  },
+  {
+    "revision": "cf189a143923b9c6a3b5b7921d8bd83e",
+    "url": "/on/static/media/Resume.cf189a14.pdf"
+  },
+  {
+    "revision": "88c4353efb1944319ea8a878dff2617b",
+    "url": "/on/static/media/black_dot.88c4353e.jpg"
+  },
+  {
+    "revision": "ac559bc66fed3b21ddee080622b7cf63",
+    "url": "/on/static/media/Cory-Bond-Logo.ac559bc6.png"
+  },
+  {
+    "revision": "09c1ffcd91ead6673761de936819397a",
+    "url": "/on/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CoryBond.github.io/precache-manifest.829a060a8e7f595378bd615e9d3f41fd.js"
+  "/on/precache-manifest.b059832bd821caae68308ee845aea269.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/CoryBond.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/on/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
